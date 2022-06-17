f3a8d6261ca3c2fd31db10523b811482b65868be
Athar Mahboob <47472554+SMAthar@users.noreply.github.com>
2022-06-17 23:52:36 +0530

    details component

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,7 +1,61 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { useParams } from "react-router-dom";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
+import { fetchData, exerciseOptions, youtubeOptions } from "../utils/fetchData";
 
 const ExerciseDetails = () => {
-  return <div>ExerciseDetails</div>;
+  const [exerciseDetail, setExerciseDetail] = React.useState({});
+  const [exerciseVideos, setExerciseVideos] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    const fetchExercisesData = async () => {
+      setIsLoading(true);
+      const exercisedbUrl = `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`;
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        exercisedbUrl,
+        exerciseOptions
+      );
+
+      const ExerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+
+      setExerciseDetail(exerciseDetailData);
+      setExerciseVideos(ExerciseVideosData);
+      setIsLoading(false);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  if (isLoading) {
+    return (
+      <Box>
+        <>Loading...</>
+      </Box>
+    );
+  } else {
+    return (
+      <Box>
+        <Detail exerciseDetail={exerciseDetail} />
+        <ExerciseVideos
+          exerciseVideos={exerciseVideos}
+          name={exerciseDetail.name}
+        />
+        <SimilarExercises />
+      </Box>
+    );
+  }
 };
 
 export default ExerciseDetails;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0e53a0dfddmshb478ada09e00642p118dbbjsn21027bdcf79d",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
